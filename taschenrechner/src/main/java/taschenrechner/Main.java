355f9d9001c11d5452d8c14213c7eb078482511d
Emre Kaplan <kaplan.emre@fh-swf.de>
2025-12-04 14:14:37 +0100

    einfachen Taschenrechner erstellen

---
+++ taschenrechner/src/main/java/taschenrechner/Main.java
@@ -0,0 +1,12 @@
+package taschenrechner;
+
+/**
+ * Main Klasse zum Ausführen des Projekts
+ */
+public class Main {
+    public static void main(String[] args) {
+        UI konsole= new UI();
+        konsole.starte();
+    }
+}
+
